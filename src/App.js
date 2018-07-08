e53b5388898947d9da4e338c45ffdbf93d6d0df5
bhanurakesh <bhanurekha.vanam@gmail.com>
2018-07-08 21:52:29 +0530

    Moving 3rd Version changes to First branch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -65,16 +65,8 @@ class App extends Component {
 
   render() {
 
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -88,35 +80,34 @@ class App extends Component {
               changed={(event) => this.nameChangedHandler(event, prsn.id)} />
           })
           }
-
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
 
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1> Hi I am React App! </h1>
-        <p className={classes.join(' ')}>   This is really working </p>
+        <p className={assignedClasses.join(' ')}>   This is really working </p>
         <button
-          style={style}
           onClick={this.switchNameHandler.bind(this, 'Maximillian')}
           key="key1">
           Switch User </button>
         <button onClick={() => this.switchNameHandler('Bhanu!!')}
           key="key2">
           Other way switch </button>
-        <button style={style}
+        <button
+          className={btnClass}
           onClick={this.togglePersonsHandler}
           key="key3">
           Toggle User
